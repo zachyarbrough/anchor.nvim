2a109f80ed88ed4696b344baeb61c5ee0a029906
ZachYarbrough <zachyarbro@gmail.com>
2026-06-17 22:04:19 -0500

    add: basic functionality for adding/deleting/viewing anchors

--- lua/anchor/init.lua
+++ lua/anchor/init.lua
@@ -34,48 +34,95 @@ end
 M.setup = function(opts)
     opts = opts or {}
 
-    vim.api.nvim_create_user_command('AnchorSetDir', function(cmd_opts)
-	M.set_dir(cmd_opts.args)
+    vim.api.nvim_create_user_command('AnchorAddDir', function(cmd_opts)
+	M.add_dir(cmd_opts.args)
     end, {
 	nargs= 1,
-	desc = 'Set the anchored directory for the current project'
+	desc = 'Attach an anchored directory to the cwd'
     })
 
-    vim.api.nvim_create_user_command('AnchorGetDir', function()
-	M.get_dir()
+    vim.api.nvim_create_user_command('AnchorDelDir', function(cmd_opts)
+	M.del_dir(cmd_opts.args)
     end, {
-	desc = 'Get the anchored directory for the current working directory'
+	nargs= 1,
+	desc = 'Delete the anchored directory attached to the cwd'
+    })
+
+    vim.api.nvim_create_user_command('AnchorList', function()
+	M.toggle_list()
+    end, {
+	desc = 'View a list of the anchored directories attached to the cwd'
     })
 
 end
 
---- Get the anchored directory associated with the current working directory
+--- Get the anchored directory associated with the cwd
 --- @return string|nil: The stored anchored directory path 
 M.load = function()
     local data = load()
 
-    return data[vim.fn.getcwd()]
+    return data[vim.uv.cwd()]
 end
 
---- Define an anchored directory for the current working directory
+--- Define an anchored directory for the cwd
 --- @param dir string: Path to the anchored directory
-M.set_dir = function(dir)
+M.add_dir = function(dir)
+    local cur_dir = vim.uv.cwd()
     local data = load()
 
-    data[vim.fn.getcwd()] = vim.fn.expand(dir)
+    -- Initialize the table for cur_dir if it doesn't exist yet
+    if not data[cur_dir] then
+        data[cur_dir] = {}
+    end
+
+    -- Add new directory to the current list
+    table.insert(data[cur_dir], vim.fn.expand(dir))
 
     save(data)
+
+    print(data[cur_dir])
 end
 
---- Get the anchored directory associated with the current working directory
-M.get_dir = function()
+--- Get the anchored directory associated with the cwd
+M.del_dir = function(dir_idx)
     local data = load()
 
-    local dir = data[vim.fn.getcwd()]
+    local removed_dir = table.remove(data[vim.uv.cwd()], dir_idx)
 
-    print(dir) -- Temporary until there is a better way of displaying
+    save(data)
 
-    return dir
+    print("Removed directory: " .. removed_dir) -- Temporary print for testing until buffer is created 
+end
+
+M.toggle_list = function()
+    local cur_dir = vim.uv.cwd()
+    local data = load()
+
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    local width = 60
+    local height = 15
+
+    local win_opts = {
+	relative = 'editor',
+	width = width,
+	height = height,
+	row = math.floor((vim.o.lines - height) / 2),
+	col = math.floor((vim.o.columns - width) / 2),
+	style = 'minimal',
+	border = 'rounded',
+    }
+
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, data[cur_dir])
+
+    local win = vim.api.nvim_open_win(buf, true, win_opts)
+
+    -- Close window with q or esc
+    for _, key in ipairs({ "q", "<esc>" }) do
+    vim.keymap.set("n", key, function()
+        vim.api.nvim_win_close(win, true)
+    end, { buffer = buf })
+end
 end
 
 return M
